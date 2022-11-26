5a78e92a4ad5f3673e935dd41235608b29d873b5
[juliana] <[juli2345@icloud.com]>
2022-11-26 03:52:21 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (70491089108, 1, 'Hello absent students!');
+
+--UPDATE sandbox SET double_value = -2.1543058 WHERE id = 124;
+
+--DELETE FROM sandbox WHERE id= 110;
+
+SELECT * FROM sandbox;
